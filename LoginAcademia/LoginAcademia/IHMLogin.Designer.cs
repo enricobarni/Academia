cafdee6650639664586f4de1ffb0058cafd8988a
Giovani <giovanisteiger@gmail.com>
2026-05-16 17:23:18 -0300

    Login_Cadastro_Design

---
+++ LoginAcademia/LoginAcademia/IHMLogin.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace LoginAcademia
+{
+    partial class IHMLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnLogin = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbOuLogin = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.btnCriarConta = new Guna.UI2.WinForms.Guna2Button();
+            this.btnEntrar = new Guna.UI2.WinForms.Guna2Button();
+            this.txtSenhaCadastro = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtNome = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lbSenhaCadastro = new System.Windows.Forms.Label();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.lbSubtitulo = new System.Windows.Forms.Label();
+            this.lbBemVindo = new System.Windows.Forms.Label();
+            this.imLogo = new System.Windows.Forms.PictureBox();
+            this.pnLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnLogin
+            // 
+            this.pnLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(15)))));
+            this.pnLogin.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.pnLogin.BorderRadius = 30;
+            this.pnLogin.BorderThickness = 2;
+            this.pnLogin.Controls.Add(this.lbOuLogin);
+            this.pnLogin.Controls.Add(this.btnCriarConta);
+            this.pnLogin.Controls.Add(this.btnEntrar);
+            this.pnLogin.Controls.Add(this.txtSenhaCadastro);
+            this.pnLogin.Controls.Add(this.txtNome);
+            this.pnLogin.Controls.Add(this.lbSenhaCadastro);
+            this.pnLogin.Controls.Add(this.lbUsuario);
+            this.pnLogin.Controls.Add(this.lbSubtitulo);
+            this.pnLogin.Controls.Add(this.lbBemVindo);
+            this.pnLogin.Controls.Add(this.imLogo);
+            this.pnLogin.Location = new System.Drawing.Point(711, 41);
+            this.pnLogin.Name = "pnLogin";
+            this.pnLogin.Size = new System.Drawing.Size(480, 600);
+            this.pnLogin.TabIndex = 5;
+            // 
+            // lbOuLogin
+            // 
+            this.lbOuLogin.AutoSize = false;
+            this.lbOuLogin.BackColor = System.Drawing.Color.Transparent;
+            this.lbOuLogin.Enabled = false;
+            this.lbOuLogin.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOuLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbOuLogin.Location = new System.Drawing.Point(225, 498);
+            this.lbOuLogin.Name = "lbOuLogin";
+            this.lbOuLogin.Size = new System.Drawing.Size(35, 24);
+            this.lbOuLogin.TabIndex = 26;
+            this.lbOuLogin.TabStop = false;
+            this.lbOuLogin.Text = "ou";
+            // 
+            // btnCriarConta
+            // 
+            this.btnCriarConta.Animated = true;
+            this.btnCriarConta.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnCriarConta.BorderRadius = 5;
+            this.btnCriarConta.BorderThickness = 2;
+            this.btnCriarConta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCriarConta.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCriarConta.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCriarConta.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCriarConta.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCriarConta.FillColor = System.Drawing.Color.Transparent;
+            this.btnCriarConta.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarConta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnCriarConta.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCriarConta.Location = new System.Drawing.Point(40, 524);
+            this.btnCriarConta.Name = "btnCriarConta";
+            this.btnCriarConta.Size = new System.Drawing.Size(400, 45);
+            this.btnCriarConta.TabIndex = 6;
+            this.btnCriarConta.TabStop = false;
+            this.btnCriarConta.Text = "CRIAR CONTA";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Animated = true;
+            this.btnEntrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnEntrar.BorderRadius = 5;
+            this.btnEntrar.BorderThickness = 2;
+            this.btnEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEntrar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnEntrar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnEntrar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnEntrar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnEntrar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnEntrar.FocusedColor = System.Drawing.Color.Transparent;
+            this.btnEntrar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEntrar.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnEntrar.Location = new System.Drawing.Point(41, 453);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(200)))));
+            this.btnEntrar.ShadowDecoration.Enabled = true;
+            this.btnEntrar.Size = new System.Drawing.Size(400, 45);
+            this.btnEntrar.TabIndex = 5;
+            this.btnEntrar.TabStop = false;
+            this.btnEntrar.Text = "ENTRAR";
+            // 
+            // txtSenhaCadastro
+            // 
+            this.txtSenhaCadastro.BorderColor = System.Drawing.Color.Transparent;
+            this.txtSenhaCadastro.BorderRadius = 5;
+            this.txtSenhaCadastro.BorderThickness = 2;
+            this.txtSenhaCadastro.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSenhaCadastro.DefaultText = "";
+            this.txtSenhaCadastro.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtSenhaCadastro.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtSenhaCadastro.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSenhaCadastro.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtSenhaCadastro.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(25)))));
+            this.txtSenhaCadastro.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtSenhaCadastro.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtSenhaCadastro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtSenhaCadastro.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtSenhaCadastro.IconLeft = global::LoginAcademia.Properties.Resources.iconesSenha;
+            this.txtSenhaCadastro.IconLeftOffset = new System.Drawing.Point(3, 0);
+            this.txtSenhaCadastro.IconLeftSize = new System.Drawing.Size(15, 15);
+            this.txtSenhaCadastro.IconRight = global::LoginAcademia.Properties.Resources.iconesSenha2;
+            this.txtSenhaCadastro.IconRightOffset = new System.Drawing.Point(5, 0);
+            this.txtSenhaCadastro.IconRightSize = new System.Drawing.Size(20, 15);
+            this.txtSenhaCadastro.Location = new System.Drawing.Point(40, 338);
+            this.txtSenhaCadastro.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSenhaCadastro.Name = "txtSenhaCadastro";
+            this.txtSenhaCadastro.PlaceholderText = "Digite sua senha";
+            this.txtSenhaCadastro.SelectedText = "";
+            this.txtSenhaCadastro.Size = new System.Drawing.Size(400, 45);
+            this.txtSenhaCadastro.TabIndex = 1;
+            this.txtSenhaCadastro.UseSystemPasswordChar = true;
+            // 
+            // txtNome
+            // 
+            this.txtNome.BorderColor = System.Drawing.Color.Transparent;
+            this.txtNome.BorderRadius = 5;
+            this.txtNome.BorderThickness = 2;
+            this.txtNome.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNome.DefaultText = "";
+            this.txtNome.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtNome.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtNome.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNome.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNome.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(25)))));
+            this.txtNome.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtNome.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtNome.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtNome.IconLeft = global::LoginAcademia.Properties.Resources.iconesUsuario2;
+            this.txtNome.IconLeftOffset = new System.Drawing.Point(5, 0);
+            this.txtNome.IconLeftSize = new System.Drawing.Size(15, 15);
+            this.txtNome.Location = new System.Drawing.Point(40, 223);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.PlaceholderText = "Digite seu nome";
+            this.txtNome.SelectedText = "";
+            this.txtNome.Size = new System.Drawing.Size(400, 45);
+            this.txtNome.TabIndex = 0;
+            // 
+            // lbSenhaCadastro
+            // 
+            this.lbSenhaCadastro.AutoSize = true;
+            this.lbSenhaCadastro.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenhaCadastro.ForeColor = System.Drawing.Color.Gray;
+            this.lbSenhaCadastro.Location = new System.Drawing.Point(37, 309);
+            this.lbSenhaCadastro.Name = "lbSenhaCadastro";
+            this.lbSenhaCadastro.Size = new System.Drawing.Size(73, 25);
+            this.lbSenhaCadastro.TabIndex = 20;
+            this.lbSenhaCadastro.Text = "SENHA";
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lbUsuario.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsuario.ForeColor = System.Drawing.Color.Gray;
+            this.lbUsuario.Location = new System.Drawing.Point(35, 194);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(93, 25);
+            this.lbUsuario.TabIndex = 19;
+            this.lbUsuario.Text = "USUÁRIO";
+            // 
+            // lbSubtitulo
+            // 
+            this.lbSubtitulo.AutoSize = true;
+            this.lbSubtitulo.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSubtitulo.ForeColor = System.Drawing.Color.Gray;
+            this.lbSubtitulo.Location = new System.Drawing.Point(103, 150);
+            this.lbSubtitulo.Name = "lbSubtitulo";
+            this.lbSubtitulo.Size = new System.Drawing.Size(253, 17);
+            this.lbSubtitulo.TabIndex = 18;
+            this.lbSubtitulo.Text = "Faça login para continuar sua evolução.";
+            // 
+            // lbBemVindo
+            // 
+            this.lbBemVindo.AutoSize = true;
+            this.lbBemVindo.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBemVindo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbBemVindo.Location = new System.Drawing.Point(119, 100);
+            this.lbBemVindo.Name = "lbBemVindo";
+            this.lbBemVindo.Size = new System.Drawing.Size(237, 50);
+            this.lbBemVindo.TabIndex = 17;
+            this.lbBemVindo.Text = "BEM-VINDO";
+            // 
+            // imLogo
+            // 
+            this.imLogo.BackColor = System.Drawing.Color.Transparent;
+            this.imLogo.Image = global::LoginAcademia.Properties.Resources.ChatGPT_Image_14_de_mai__de_2026__17_10_07;
+            this.imLogo.Location = new System.Drawing.Point(168, 14);
+            this.imLogo.Name = "imLogo";
+            this.imLogo.Size = new System.Drawing.Size(134, 100);
+            this.imLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imLogo.TabIndex = 21;
+            this.imLogo.TabStop = false;
+            // 
+            // IHMLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LoginAcademia.Properties.Resources.ChatGPT_Image_13_de_mai__de_2026__18_06_17;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1942, 1102);
+            this.Controls.Add(this.pnLogin);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "IHMLogin";
+            this.Text = "IHMLogin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.IHMLogin_Load);
+            this.pnLogin.ResumeLayout(false);
+            this.pnLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnLogin;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbOuLogin;
+        private Guna.UI2.WinForms.Guna2Button btnCriarConta;
+        private Guna.UI2.WinForms.Guna2Button btnEntrar;
+        private Guna.UI2.WinForms.Guna2TextBox txtSenhaCadastro;
+        private Guna.UI2.WinForms.Guna2TextBox txtNome;
+        private System.Windows.Forms.Label lbSenhaCadastro;
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.Label lbSubtitulo;
+        private System.Windows.Forms.Label lbBemVindo;
+        private System.Windows.Forms.PictureBox imLogo;
+    }
+}
---
+++ LoginAcademia/LoginAcademia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginAcademia
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new IHMLogin()); //Muda o nme aqui pra qual design quer rodar
+        }
+    }
+}
